eb7841616a0412a64c8d59f75e049d60dbc68470
ClementPerot <34515947+ClementPerot@users.noreply.github.com>
2019-11-26 15:38:16 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,9 @@
 const SafeMath = artifacts.require("SafeMath");
 const ERC20 = artifacts.require("ERC20");
 
+const ticker = "OC";
+const totalSupply = 10**30;
+const decimals = 8;
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
