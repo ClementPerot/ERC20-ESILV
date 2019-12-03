44943b3bac29c4736fb6a4840014e389a7565d2a
ClementPerot <34515947+ClementPerot@users.noreply.github.com>
2019-12-03 20:53:54 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,20 @@
 const SafeMath = artifacts.require("SafeMath");
 const ERC20 = artifacts.require("ERC20");
+const MultiLevelDistribution_AirDrop = artifacts.require("MultiLevelDistribution_AirDrop")
+const WhitelistedRole = artifacts.require("WhitelistedRole")
 
-const ticker = "OC";
+
+const name = "ouiCoin"
+const ticker = "OC"
 const totalSupply = 10**30;
 const decimals = 8;
+const rate = 4;
+
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
-  deployer.deploy(ERC20);
-
+  deployer.link(SafeMath, ERC20);
+  deployer.deploy(ERC20, name, ticker, totalSupply, decimals);
+  deployer.deploy(MultiLevelDistribution_AirDrop);
+  deployer.deploy(WhitelistedRole);
 };
